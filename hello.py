7bff3be3a77087a7ead1d72c96914596ff89f782
Robert Walias <waliasrobert@yahoo.com>
2025-07-17 07:22:40 -0700

    Updated hello.py to make a web request using requests library

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
 print("Hello, Data Center")
+import requests
+response = requests.get("https://api.github.com")
+print("Status Code:", response.status_code)
+print("Content.")
+print(response.text[:500])
