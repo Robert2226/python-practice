f98cb29d5265bfcf74f179f775e851213ab44c4d
Robert Walias <waliasrobert@yahoo.com>
2025-07-17 08:49:10 -0700

    Add comment to explain Github API call

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 print("Hello, Data Center")
+#This script makes a GET request to the Github API
 import requests
 response = requests.get("https://api.github.com")
 print("Status Code:", response.status_code)
